35b894215a33a9c5cfac4964e0d01adf873a0cca
Bruno Andrade <bruno.balint@gmail.com>
2021-04-14 03:31:21 -0300

    removendo dependencia

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,2 +1,2 @@
-const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin');
-module.exports = (on,config) => { registerReportPortalPlugin(on,config); return config} 
+//const registerReportPortalPlugin = require('@reportportal/agent-js-cypress');
+//module.exports = (on,config) => { registerReportPortalPlugin(on,config); return config} 
